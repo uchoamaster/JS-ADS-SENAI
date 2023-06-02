a1ba782e1e1f8f9d519a8e48bc9aad7e9c4de147
ANA CAROLINA LAMBERTUCCI SIMAS <ana_c_simas@estudante.sesisenai.org.br>
2023-06-01 21:08:52 -0300

    primeiro commit

---
+++ missao-js/js/app.js
@@ -0,0 +1,9 @@
+function eventoOnload(){
+    console.log('Evento onLoad disparado');
+}
+
+let missaoJs = 'Missão JS em execução!';
+
+function eventoOnClick(){
+    console.log('Missão JS realizada com sucesso');
+}
