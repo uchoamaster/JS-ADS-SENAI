4dc437b6d0267a64b60f7f2ce0efe985740ee4c1
Fernandowarmling <warmlingfernando@gmail.com>
2023-06-04 20:02:34 -0300

    atividades aula ead 02-06-2026

---
+++ fernando_warmling/misao-js/js/app.js
@@ -0,0 +1,9 @@
+function eventoOnload(){
+    console.log("Evento Onload disparado")
+}
+
+let missaoJs= "Missão Js em execução!!!"
+
+function eventoOnclick(){
+    console.log("Missão JS realizada com sucesso!!!")
+}
