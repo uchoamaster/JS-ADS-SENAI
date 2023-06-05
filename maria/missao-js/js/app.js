b84388592679fccdbfc7c93fb841b278e8dce0b2
MARIA ANTONIA ROSA <maria_antonia-rosa@estudante.sesisenai.org.br>
2023-06-05 20:35:13 -0300

    Implementando atividade EAD da Maria

---
+++ maria/missao-js/js/app.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    console.log("Evento Onload disparado!")
+}
+
+function setarValor() { 
+    let missaoJs = "Missão JS em Execução!";
+   console.log(missaoJs);
+}
+
+function onclickFunction() { 
+    console.log("Missão JS realizada com sucesso!");
+}
