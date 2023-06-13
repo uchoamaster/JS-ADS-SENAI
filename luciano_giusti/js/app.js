28bc96337f3b2564926dca627e349d102740e16e
Luciano Giusti <121564508+Giusti231@users.noreply.github.com>
2023-06-13 09:29:34 -0300

    tarefa 09 concluída

--- luciano_giusti/js/app.js
+++ luciano_giusti/js/app.js
@@ -1,3 +1,6 @@
 function eventoOnload(){
-    
+    var texto = "Evento onload disparado."
+    var missaoJs = " Missão JS em execução!!!"
+    alert(texto + missaoJs);
+    return console.log(texto, missaoJs);
 }
